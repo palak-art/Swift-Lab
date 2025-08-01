7addb88c80cf53e8c5b178c080cb0f3778be04e7
palak-art <palak0676.be23@chitkara.edu.in>
2025-08-01 14:18:09 +0530

    project

---
+++ Personality_Quiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  IntroductionViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Vasu_SKH on 29/07/25.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+    }
+
+}
+
